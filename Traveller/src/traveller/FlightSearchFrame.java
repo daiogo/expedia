6946de2e288f2d1fff924528f3bea44ed26a885c
diego <diegolee7@gmail.com>
2016-05-09 19:38:34 -0300

    Flight Table is now composed of strings

--- Traveller/src/traveller/FlightSearchFrame.java
+++ Traveller/src/traveller/FlightSearchFrame.java
@@ -40,20 +40,20 @@ public class FlightSearchFrame extends javax.swing.JFrame {
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null, null, null},
-                {null, null, null, null, null, null},
-                {null, null, null, null, null, null},
-                {null, null, null, null, null, null}
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null}
             },
             new String [] {
-                "Code", "From", "Time", "To", "Time", "Price"
+                "Flight", "Airline", "From", "Time", "To", "Time", "Airfare"
             }
         ) {
             Class[] types = new Class [] {
-                java.lang.Integer.class, java.lang.Object.class, java.lang.Object.class, java.lang.Object.class, java.lang.Object.class, java.lang.Object.class
+                java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class
             };
             boolean[] canEdit = new boolean [] {
-                false, false, false, false, false, false
+                false, false, false, false, false, false, false
             };
 
             public Class getColumnClass(int columnIndex) {
